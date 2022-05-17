1af4e6f58a783c053c70f4e13c607f63eb709d24
Will <104028913+potato-potarto@users.noreply.github.com>
2022-05-17 20:00:56 +0100

    dom magic

--- main.js
+++ main.js
@@ -4,9 +4,17 @@ function roundUp(x) {
 }
 
 function CalculateMagic(a, b) {
-    // add your code here
-    
-    
-    // remember to call roundUp() with your result before returning
-    // return roundUp(g);
+    let number1 = a + b + b + 90;
+    let number2 = number1 / 10;
+    if (number2 % 5 === 0){
+        number3 = number2 - 8;
+    }
+    else {
+        number4 = number2 +9;
+    }
+
+let number5 = number4 * number4;
+let G = number5 / 3;
+
+return roundUp(G);
 }
